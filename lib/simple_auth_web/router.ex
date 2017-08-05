7d7ea2137b27b7f0c30be79e68aa5641503e245f
erikjohannesson <e.johannesson@gmail.com>
2017-08-05 15:51:07 +0200

    add logged in and admin routes

--- lib/simple_auth_web/router.ex
+++ lib/simple_auth_web/router.ex
@@ -19,14 +19,39 @@ defmodule SimpleAuthWeb.Router do
     plug SimpleAuth.CurrentUser
   end
 
+  pipeline :login_required do
+  end
+
+  pipeline :admin_required do
+  end
+
+  # guest zone
   scope "/", SimpleAuthWeb do
     pipe_through [:browser, :with_session] # Use the default browser stack
 
     get "/", PageController, :index
 
-    resources "/users", UserController, only: [:show, :new, :create]
+    resources "/users", UserController, only: [:new, :create]
 
     resources "/sessions", SessionController, only: [:new, :create, :delete], singleton: true
+  end
+
+  # registered user zone
+  scope "/", SimpleAuthWeb do
+    pipe_through [:browser, :with_session, :login_required]
+
+    resources "/users", UserController, only: [:show] do
+      resources "/posts", PostController
+    end
+  end
+
+  # admin zone
+  scope "/admin", SimpleAuthWeb.Admin, as: :admin do
+    pipe_through [:browser, :with_session, :login_required, :admin_required]
+
+    resources "/users", UserController, only: [:index, :show] do
+      resources "/posts", PostController, only: [:index, :show]
+    end
   end
 
   # Other scopes may use custom stacks.
